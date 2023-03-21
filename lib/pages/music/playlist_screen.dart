dc2ef78022510c18d33e6b510ef880a087221cdd
Gideon Salami <92346444+SalamiTech@users.noreply.github.com>
2023-03-21 20:59:27 +0000

    music 2.5/3

---
+++ lib/pages/music/playlist_screen.dart
@@ -0,0 +1,24 @@
+// import 'package:flutter/material.dart';
+
+// import '../../ models/playlist_model.dart';
+
+// class PlaylistScreen extends StatelessWidget {
+//   const PlaylistScreen({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//    Playlist playlist = Playlist.playlists[0];
+
+//     return Scaffold(
+//       backgroundColor: Colors.white,
+//       body: SingleChildScrollView(
+//         child: Column(
+//           children: [
+//             Image.network(plau)
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
