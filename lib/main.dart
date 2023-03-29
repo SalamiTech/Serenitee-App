68e79c3f620d8e44bbbb8816329c940869453219
Gideon Salami <92346444+SalamiTech@users.noreply.github.com>
2023-03-29 07:23:13 +0000

    adding new pages

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const HomeScreen(),
+      home: const OnBoardingScreen(),
       getPages: [
         GetPage(name: '/', page: () => const MusicSelectScreen()),
         GetPage(name: '/song', page: () => const SongScreenPage()),
